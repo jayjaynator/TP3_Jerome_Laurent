e9e6abcefd8a4583ac6a3c03288269dcf5911837
Britt1eBones <butterlord47@gmail.com>
2021-05-01 17:31:32 -0400

    suppression du doublon

--- TP3_FlappyPapillon/TP1Ferme/Constants.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-const char* const WINDOW_TITLE = "La Mini-Ferme de la Mort";
-const int SCREEN_WIDTH = 768;
-const int SCREEN_HEIGHT = 512;
--- TP3_FlappyPapillon/TP1Ferme/Input.cpp
+++
@@ -1,4 +0,0 @@
-#include "Input.h"
-#include <array>
-
-std::array<bool, 5> Input::PressedKeys{false, false, false, false, false};
